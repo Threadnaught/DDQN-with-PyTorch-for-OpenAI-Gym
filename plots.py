500ce02e1872aa756caced790e706e648deb45cc
idk <patrickhingley@live.co.uk>
2024-12-08 18:16:23 +0000

    added generic imshow

--- plots.py
+++ plots.py
@@ -37,57 +37,6 @@ epsilons = np.load('saved-double/epsilons.npy')
 def show_reset_cartpole():
     env.render()
 
-def show_ang_ang_vel_img():
-    # ang_abs = 0.25
-
-    angs = np.arange(-0.25, 0.25, 0.25 / 10, dtype=np.float32)
-    # ang_vels = np.arange(3, -3, 3 / 10, dtype=np.float32)
-    ang_vels = np.linspace(3,-3, 20, dtype=np.float32)
-
-    angs_grid, ang_vels_grid = np.meshgrid(angs, ang_vels)
-
-    # print(angs_grid)
-    # exit()
-
-    # print(angs_grid.shape, ang_vels_grid.shape)
-    # exit()
-
-    zeros = np.zeros_like(angs_grid, dtype=np.float32)
-
-    states = torch.permute(torch.asarray([zeros, zeros, angs_grid, ang_vels_grid]), [1,2,0])
-    states_flat = torch.reshape(states, [-1, env.observation_space.shape[0]])
-
-    output_q1 = Q1(states_flat).detach()
-    # output_q2 = Q2(states_flat).detach()
-
-    # plt.plot(angs, output_q1[:,0])
-    # plt.plot(angs, output_q1[:,1])
-
-    output_q1_shaped = torch.reshape(output_q1, list(angs_grid.shape[:2]) +[-1])
-
-    print(output_q1_shaped.shape)
-
-    chosen_action = output_q1_shaped[:,:,0] - output_q1_shaped[:,:,1]
-
-    # Nicked from mpl docs
-    top = cm.get_cmap('Oranges', 128)
-    bottom = cm.get_cmap('Blues', 128)
-
-    newcolors = np.vstack((top(np.linspace(1, 0.33, 128)),
-                        bottom(np.linspace(0.33, 1, 128))))
-    newcmp = colors.ListedColormap(newcolors, name='OrangeBlue')
-
-
-    plt.title('Pole Angle v Angular Velocity')
-
-    plt.imshow(chosen_action.detach(), cmap=newcmp, vmax=25, vmin=-25, extent=[-0.25,0.25,-3,3], aspect='auto')
-    plt.colorbar(label='Q(s,a0) - Q(s,1)')
-    plt.xlabel('Pole Angle')
-    plt.ylabel('Pole Angular Velocity')
-
-    plt.show()
-    # print(angs)
-
 def show_episode_reward_graph():
     q_is = np.unique(episode_rewards[:,0])
     i_qs = {int(q_is[i]):i for i in range(len(q_is))}
@@ -110,19 +59,6 @@ def show_episode_reward_graph():
 
     
     plt.show()
-    
-    # print(q_ep_rewards[0][:10])
-    
-    # ep_rewards_split = np.reshape(episode_rewards, [-1, 2])
-    # q_1_rewards = ep_rewards_split[:,0]
-    # q_2_rewards = ep_rewards_split[:,1]
-
-    # plt.plot(np.arange(0,len(episode_rewards),2), q_1_rewards, 'o', color='#FFA50088', markeredgewidth=0)
-    # plt.plot(np.arange(1,len(episode_rewards),2), q_2_rewards, 'o', color='#00FFA588', markeredgewidth=0)
-    
-    # ep_rewards_moving_average = np.convolve(episode_rewards, np.ones(100)/100, mode='valid')
-    # plt.plot(np.arange(len(ep_rewards_moving_average))+99, ep_rewards_moving_average)
-    # plt.show()
 
 def reward_peformance_graph(chosen_q):
     q_is = np.unique(rewards[:,0])
@@ -220,38 +156,82 @@ def plot_q_angles(Q):
     plt.legend()
     plt.ylim(-150,135)
 
-reward_peformance_graph(0)
-plt.savefig('images/r-p-graph-q1.png')
-plt.clf()
 
-reward_peformance_graph(1)
-plt.savefig('images/r-p-graph-q2.png')
-plt.clf()
+def imshow_q_generic(states, extent):
 
-performance_comparison_graph()
-plt.savefig('images/p-graph-both-qs.png')
-plt.clf()
+    states_flat = torch.reshape(states, [-1, env.observation_space.shape[0]])
 
-for pos in [-1,0,1]:
-    plot_q_overestimation(pos)
-    plt.savefig('images/q-overestimation-%i.png' % pos)
-    # plt.show()
-    plt.clf()
+    output_q1 = Q1(states_flat).detach()
 
-plot_q_angles(Q1_1000_episodes)
-plt.savefig('images/q-1000-episodes.png')
-plt.clf()
+    output_q1_shaped = torch.reshape(output_q1, list(states.shape[:2]) + [2])
 
+    chosen_action = (output_q1_shaped[:,:,0] - output_q1_shaped[:,:,1]).detach().numpy()
 
-plot_q_angles(Q1_1000_episodes)
-plt.savefig('images/q-2000-episodes.png')
-plt.clf()
+    color_extent = np.max(np.abs([x(chosen_action) for x in [np.min, np.max]]))
+
+    # Nicked from mpl docs
+    top = cm.get_cmap('Oranges', 128)
+    bottom = cm.get_cmap('Blues', 128)
+
+    newcolors = np.vstack((top(np.linspace(1, 0.33, 128)),
+                        bottom(np.linspace(0.33, 1, 128))))
+    newcmp = colors.ListedColormap(newcolors, name='OrangeBlue')
+
+    plt.imshow(chosen_action, cmap=newcmp, vmax=color_extent, vmin=-color_extent, extent=extent, aspect='auto')
+    plt.colorbar(label='Q(s,a0) - Q(s,1)')
 
 
-plot_q_angles(Q1)
-plt.savefig('images/q-3000-episodes.png')
-plt.clf()
+def ang_ang_vel_img():
+    # ang_abs = 0.25
 
-# ep 1000: 0.367695 2000: 0.135200 3000: 0.049762
-print('ep 1000: %f 2000: %f 3000: %f' % (epsilons[1000], epsilons[2000], epsilons[-1]))
+    angs = np.linspace(-0.25, 0.25, 10, dtype=np.float32)
+    ang_vels = np.linspace(3,-3, 20, dtype=np.float32)
+
+    angs_grid, ang_vels_grid = np.meshgrid(angs, ang_vels)
+    
+    zeros = np.zeros_like(angs_grid, dtype=np.float32)
+
+    states = torch.permute(torch.asarray([zeros, zeros, angs_grid, ang_vels_grid]), [1,2,0])
+
+    imshow_q_generic(states, [-0.25,0.25,-3,3])
+
+    plt.xlabel('Pole Angle')
+    plt.ylabel('Pole Angular Velocity')
+
+
+
+
+# reward_peformance_graph(0)
+# plt.savefig('images/r-p-graph-q1.png')
+# plt.clf()
+
+# reward_peformance_graph(1)
+# plt.savefig('images/r-p-graph-q2.png')
+# plt.clf()
+
+# performance_comparison_graph()
+# plt.savefig('images/p-graph-both-qs.png')
+# plt.clf()
+
+# for pos in [-1,0,1]:
+#     plot_q_overestimation(pos)
+#     plt.savefig('images/q-overestimation-%i.png' % pos)
+#     # plt.show()
+#     plt.clf()
+
+# plot_q_angles(Q1_1000_episodes)
+# plt.savefig('images/q-1000-episodes.png')
+# plt.clf()
+# plot_q_angles(Q1_1000_episodes)
+# plt.savefig('images/q-2000-episodes.png')
+# plt.clf()
+# plot_q_angles(Q1)
+# plt.savefig('images/q-3000-episodes.png')
+# plt.clf()
+
+# # ep 1000: 0.367695 2000: 0.135200 3000: 0.049762
+# print('ep 1000: %f 2000: %f 3000: %f' % (epsilons[1000], epsilons[2000], epsilons[-1]))
 # plt.show()
+
+ang_ang_vel_img()
+plt.show()
